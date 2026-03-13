33bbc64ee1283486278ad2c49e5db4c2d013f6ac
Alexandra Miresan <amiresan@gmail.com>
2026-03-14 00:52:44 +0200

    Update TerminalLine
    Add list of TerminalCell, and getter and setter for the list and for individual cell

--- src/main/java/TerminalLine.java
+++ src/main/java/TerminalLine.java
@@ -1,2 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class TerminalLine {
+    private List<TerminalCell> cells;
+
+    public TerminalLine(int width){
+        this.cells = new ArrayList<>();
+
+        for(int i = 0; i < width; i++){
+            this.cells.add(new TerminalCell());
+        }
+    }
+
+    public List<TerminalCell> getLine() {
+        return cells;
+    }
+
+    public void setLine(List<TerminalCell> line) {
+        this.cells = line;
+    }
+
+    public TerminalCell getCell(int index){
+        return this.cells.get(index);
+    }
+
+    public void setCell(int index, TerminalCell cell){
+        this.cells.set(index, cell);
+    }
+
 }
