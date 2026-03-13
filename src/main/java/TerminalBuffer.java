72a49b58bfc5ca7289ca1256dfe67bbce28f8302
Alexandra Miresan <amiresan@gmail.com>
2026-03-14 01:00:39 +0200

    Update TerminalBuffer
    Add width, height, cursorX, cursorY, scrollbackMaxSize, screen, scrollback and currentAttributes.
    Add cursor getters and setter

--- src/main/java/TerminalBuffer.java
+++ src/main/java/TerminalBuffer.java
@@ -1,2 +1,44 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class TerminalBuffer {
+    private int width;
+    private int height;
+
+    private int cursorX = 0;
+    private int cursorY = 0;
+
+    private int scrollbackMaxSize;
+
+    private List<TerminalLine> screen;
+    private List<TerminalLine> scrollback;
+
+    private Attributes currentAttributes;
+
+    public TerminalBuffer(int width, int height, int scrollbackMaxSize) {
+        this.width = width;
+        this.height = height;
+        this.scrollbackMaxSize = scrollbackMaxSize;
+
+        this.screen = new ArrayList<>();
+        this.scrollback = new ArrayList<>();
+
+        for(int i = 0; i < height; i++){
+            screen.add(new TerminalLine(width));
+        }
+    }
+
+    public int getCursorX() {
+        return cursorX;
+    }
+
+    public int getCursorY() {
+        return cursorY;
+    }
+
+    public void setCursor(int cursorX, int cursorY) {
+        this.cursorX = Math.max(0, Math.min(width - 1, cursorX));
+        this.cursorY = Math.max(0, Math.min(height - 1, cursorY));
+    }
+
 }
