7d6e964811c58ebd51f45d6c5344ea107cd7e2cf
Alexandra Miresan <amiresan@gmail.com>
2026-03-14 00:44:02 +0200

    Update TerminalCell
    Add character and attributes variables, and getters and setters

--- src/main/java/TerminalCell.java
+++ src/main/java/TerminalCell.java
@@ -1,2 +1,27 @@
 public class TerminalCell {
+    private char character;
+
+    private Attributes attributes;
+
+    public TerminalCell(char character, Attributes attributes) {
+        this.character = character;
+        this.attributes = attributes;
+    }
+
+    public char getCharacter() {
+        return character;
+    }
+
+    public void setCharacter(char character) {
+        this.character = character;
+    }
+
+    public Attributes getAttributes() {
+        return attributes;
+    }
+
+    public void setAttributes(Attributes attributes) {
+        this.attributes = attributes;
+    }
+    
 }
