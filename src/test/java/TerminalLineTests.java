b3299e2a611870616d6fa18057fb405a41f932b1
Alexandra Miresan <amiresan@gmail.com>
2026-03-15 13:21:17 +0200

    Update TerminalLineTests - default constructor
    Add testDefaultConstructor() function for testing

--- src/test/java/TerminalLineTests.java
+++ src/test/java/TerminalLineTests.java
@@ -4,7 +4,18 @@ import static org.junit.jupiter.api.Assertions.*;
 
 
 class TerminalLineTests {
+
     public int getLineSize(TerminalLine line){
         return line.getLine().size();
+    }
+
+    @Test
+    void testDefaultConstructor(){
+        TerminalLine line = new TerminalLine(5);
+        assertEquals(5, getLineSize(line));
+
+        for(int i = 0; i < getLineSize(line); i++){
+            assertEquals(' ', line.getCell(i).getCharacter());
+        }
     }
 }
