2b59116fdce7ab07100e628c3f5ef32f51df9d91
Alexandra Miresan <amiresan@gmail.com>
2026-03-15 13:06:36 +0200

    Update AttributesTests - default constructor
    Add testDefaultConstructor() function for testing

--- src/test/java/AttributesTests.java
+++ src/test/java/AttributesTests.java
@@ -1,4 +1,18 @@
-public class AttributesTests {
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+
+class AttributesTest {
+
+    @Test
+    void testDefaultConstructor()
+    {
+        Attributes attributes = new Attributes();
+        assertEquals(Color.DEFAULT, attributes.getForeground());
+        assertEquals(Color.DEFAULT, attributes.getBackground());
+
+        assertFalse(attributes.isBold());
+        assertFalse(attributes.isItalic());
+        assertFalse(attributes.isUnderline());
+    }
+
 }
