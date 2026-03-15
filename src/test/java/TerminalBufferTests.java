13b117cf39182889b106e338837f0c8726a1efde
Alexandra Miresan <amiresan@gmail.com>
2026-03-15 13:57:51 +0200

    Update TerminalBufferTests - cursor movement
    Add testCursorMovement() function which tests all the ways the cursor can move

--- src/test/java/TerminalBufferTests.java
+++ src/test/java/TerminalBufferTests.java
@@ -29,3 +29,21 @@ class TerminalBufferTest {
         assertEquals(0, buffer.getCursorX());
         assertEquals(0, buffer.getCursorY());
     }
+
+    @Test
+    void testCursorMovement() {
+        TerminalBuffer buffer = new TerminalBuffer(5,3,5);
+
+        buffer.moveCursorRight(3);
+        assertEquals(3, buffer.getCursorX());
+
+        buffer.moveCursorLeft(2);
+        assertEquals(1, buffer.getCursorX());
+
+        buffer.moveCursorDown(2);
+        assertEquals(2, buffer.getCursorY());
+
+        buffer.moveCursorUp(1);
+        assertEquals(1, buffer.getCursorY());
+    }
+
