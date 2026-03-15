a304fc3d5b69ece2da552ebf71a9c97da6a0fbb0
Alexandra Miresan <amiresan@gmail.com>
2026-03-15 13:54:47 +0200

    Update TerminalBufferTests - default constructor
    Add testDefaultConstructor() function for testing

--- src/test/java/TerminalBufferTests.java
+++ src/test/java/TerminalBufferTests.java
@@ -2,3 +2,16 @@ public class TerminalBufferTests {
 }
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+
+class TerminalBufferTest {
+
+    @Test
+    void testDefaultConstructor() {
+        TerminalBuffer buffer = new TerminalBuffer(4, 3, 5);
+
+        assertEquals(0, buffer.getCursorX());
+        assertEquals(0, buffer.getCursorY());
+
+        assertEquals(' ', buffer.getCharacterAtPosition(0,0));
+    }
+
