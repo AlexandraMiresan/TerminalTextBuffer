fd2c81bcd1075afed0bd793bd1f2b293ffd271a0
Alexandra Miresan <amiresan@gmail.com>
2026-03-15 13:13:19 +0200

    Update TerminalCell - default constructor
    Add testDefaultConstructor() function for testing

--- src/test/java/TerminalCellTests.java
+++ src/test/java/TerminalCellTests.java
@@ -1,3 +1,19 @@
 public class TerminalCellTests {
 }import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+class TerminalCellTests {
+
+    @Test
+    void testDefaultConstructor() {
+        TerminalCell terminalCell = new TerminalCell();
+        Attributes attributes = new Attributes();
+
+        assertEquals(' ', terminalCell.getCharacter());
+        assertEquals(Color.DEFAULT, attributes.getForeground());
+        assertEquals(Color.DEFAULT, attributes.getBackground());
+
+        assertFalse(attributes.isBold());
+        assertFalse(attributes.isItalic());
+        assertFalse(attributes.isUnderline());
+    }
+
